dc9b1d888c55b242fd124645ca88dce15b712a63
Karthi-Rajendran <karthikrajan06@gmail.com>
2019-10-04 12:47:21 +0530

    Commit Js

---
+++ JavaScript/myscripts/jlc.js
@@ -0,0 +1,9 @@
+var mydiv3=document.getElementById("my-div3");
+            mydiv3.addEventListener("click",function(){
+                alert('Mongo has been clicked.');
+            });
+
+var mydiv4=document.getElementById("my-div4");
+            mydiv4.addEventListener("dblclick",function(){
+                alert('NodeJs Training has been double clicked');
+            });
